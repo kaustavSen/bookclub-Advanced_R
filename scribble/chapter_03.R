0b0ad6b49701728bd421e47076872b986b34aca0
Kaustav Sen <sen_kaustav@hotmail.com>
2020-05-03 14:05:43 +0530

    WIP

--- scribble/chapter_03.R
+++ scribble/chapter_03.R
@@ -57,22 +57,64 @@ df3[c(1, 1, 1), ]
 
 as_tibble(df3, rownames = "name")
 
+df1 <- data.frame(xyz = "a")
+df2 <- tibble(xyz = "a")
 
+str(df1$x)
+str(df2$x)
 
+data.frame(
+  x = 1:3,
+  y = list(1:3, 2:4, 3:5)
+)
 
+dfm <- data.frame(
+  x = 1:3 * 10
+)
 
+dfm
+dfm$y <- matrix(1:9, nrow = 3)
+dfm$z <- data.frame(a = 1:3, b = letters[1:3], stringsAsFactors = FALSE)
 
+str(dfm)
 
+as_tibble(dfm)
 
+df <- data.frame(
+  a = rep(c("apple", "banana"), 5),
+  b = 1:10 * runif(10)
+)
 
+rownames(df) <- rep(1, 10)
 
+t(df)
+t(t(df))
 
+data.matrix(df)
 
+df_coltypes <- data.frame(
+  a = c("a", "b"),
+  b = c(TRUE, FALSE),
+  c = c(1L, 0L),
+  d = c(1.5, 2),
+  e = c("one" = 1, "two" = 2),
+  g = factor(c("f1", "f2")),
+  stringsAsFactors = FALSE
+)
 
+data.matrix(df_coltypes)
 
+x <- NULL
 
+length(x)
+attributes(x)
+attr(x, "my_attribute") <- 123
 
+x <- list(a = 1:2, b = c("apple", "orange", "banana"))
 
+str(x)
 
+dim(x) <- c(1, 2)
 
+x
 
